d23eb1c195257ce86c73d7a6a321b8331f002b7e
Javieer E <65838690+Javieer57@users.noreply.github.com>
2022-03-08 22:59:32 -0600

    Add reload page functionality

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const adviceBtn = document.getElementById('adviceBtn');
+
+const loadAdvice = async () => {
+	try {
+		let advice = await fetch('https://api.adviceslip.com/advice');
+
+		let data = await advice.json();
+		console.log(data);
+
+		let adviceID = data.slip.id;
+		let adviceText = data.slip.advice;
+
+		document.getElementById('adviceID').innerHTML = adviceID;
+		document.getElementById('adviceText').innerHTML = adviceText;
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+adviceBtn.addEventListener('click', () => {
+	location.reload();
+});
+
+loadAdvice();
