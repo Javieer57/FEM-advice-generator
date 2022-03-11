06abcc81f7dca33ce5410a86df3c3a4a97f131d9
Javieer E <65838690+Javieer57@users.noreply.github.com>
2022-03-11 15:46:25 -0600

    Optimize main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 const adviceBtn = document.getElementById('adviceBtn');
+const adviceID = document.getElementById('adviceID');
+const adviceText = document.getElementById('adviceText');
 
 const loadAdvice = async () => {
-	document.getElementById('adviceText').innerHTML = `...`;
+	adviceID.innerHTML = '';
+	adviceText.innerHTML = '...';
+
 	try {
 		let advice = await fetch('https://api.adviceslip.com/advice', { cache: 'reload' });
+
+		/* This advice has a bug related to charset */
 		// let advice = await fetch('https://api.adviceslip.com/advice/146', { cache: 'reload' });
 
 		let data = await advice.json();
 		console.log(data);
 
-		let adviceID = data.slip.id;
-		let adviceText = data.slip.advice;
-
-		document.getElementById('adviceID').innerHTML = adviceID;
-		document.getElementById('adviceText').innerHTML = `“${adviceText}”`;
+		adviceID.innerHTML = data.slip.id;
+		adviceText.innerHTML = `“${data.slip.advice}”`;
 	} catch (error) {
+		adviceID.innerHTML = '0';
+		adviceText.innerHTML = `<i>An error has occurred, please try again.</i>`;
 		console.log(error);
 	}
 };
 
 adviceBtn.addEventListener('click', () => {
-	// location.reload();
 	loadAdvice();
 });
 
